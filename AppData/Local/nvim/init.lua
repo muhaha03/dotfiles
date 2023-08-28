d6623f50494993317ae0f8f40fcc79ddc7a1f4f9
muhaha03 <muhaha03@gmail.com>
2023-08-28 19:04:42 +0800

    Update AppData/Local/nvim/init.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -529,7 +529,7 @@ cmp.setup {
 -- muhaha03
 vim.keymap.set("n", "<F2>", ":NvimTreeToggle D: <cr>",{ silent=true} )
 vim.cmd([[
-  au BufReadPost,BufRead,BufNewFile,BufWinEnter *.txt,*.rsp,*.cfg,*.ini,*.log,*.*.log set syntax=sh
+  au BufReadPost,BufRead,BufNewFile,BufWinEnter *.txt,*.rsp,*.cfg,*.ini set syntax=sh
   au BufReadPost,BufRead,BufNewFile,BufWinEnter *.log,*.*.log set syntax=sql
 --  au BufReadPost,BufRead,BufNewFile,BufWinEnter *.rsp set syntax=sh
 --  au BufReadPost,BufRead,BufNewFile,BufWinEnter *.cfg set syntax=sh
